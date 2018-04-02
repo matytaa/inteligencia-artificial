410b9bbf4f8044301cbee316efb42e15196a25a1
Matias Nievas Soto <matytaa@gmail.com>
2018-04-02 17:40:31 -0300

    agregando método intercambiar

--- Sliding-Puzzle/SlidingPuzzle.py
+++ Sliding-Puzzle/SlidingPuzzle.py
@@ -1,9 +1,11 @@
 class SlidingPuzzle:
 	matrizPuzzle = [[]]
 	filaMostrar = [0,0,0]
+	filaColumnas = 0
 
-	def __init__(self, matrizPuzzle):
+	def __init__(self, matrizPuzzle, dimension):
 		self.matrizPuzzle = matrizPuzzle
+		self.filaColumnas = dimension
 
 	def mostrarPuzzle(self):
 		for i in range(3):
@@ -11,12 +13,30 @@ class SlidingPuzzle:
 				self.filaMostrar[j] = self.matrizPuzzle[i][j]
 			print self.filaMostrar
 
+	def intercambiar(self, fila_original, columna_original, fila_nueva, columna_nueva):
+		if fila_nueva >= 0 and columna_nueva >=0 and self.filaColumnas > fila_nueva and self.filaColumnas > columna_nueva:
+			auxiliar = self.matrizPuzzle[fila_nueva][columna_nueva]
+			self.matrizPuzzle[fila_nueva][columna_nueva] = 0
+			self.matrizPuzzle[fila_original][columna_original] = auxiliar
+		else:
+			print("movimiento no valido")
+
 	def down(self, fila, columna):
-		auxiliar = self.matrizPuzzle[fila+1][columna]
-		self.matrizPuzzle[fila+1][columna] = 0
-		self.matrizPuzzle[fila][columna] = auxiliar
+		self.intercambiar(fila,columna, fila+1, columna)
+		print("\n intercambio abajo")
+		self.mostrarPuzzle()
+
+	def up(self, fila, columna):
+		self.intercambiar(fila,columna, fila-1, columna)
+		print("\n intercambio arriba")
+		self.mostrarPuzzle()
 
 	def left(self, fila, columna):
-		auxiliar = self.matrizPuzzle[fila][columna-1]
-		self.matrizPuzzle[fila][columna-1] = 0
-		self.matrizPuzzle[fila][columna] = auxiliar
+		self.intercambiar(fila,columna, fila, columna-1)
+		print("\n intercambio izquierda")
+		self.mostrarPuzzle()
+
+	def right(self, fila, columna):
+		self.intercambiar(fila,columna, fila, columna+1)
+		print("\n intercambio derecha")
+		self.mostrarPuzzle()
--- Sliding-Puzzle/test_SlidingPuzzle.py
+++ Sliding-Puzzle/test_SlidingPuzzle.py
@@ -5,7 +5,7 @@ class test_SlidingPuzzle(unittest.TestCase):
 
 	def setUp(self):
 		matrizPuzzle = [[1,0,5],[6,4,7],[2,3,8]]
-		self.miPuzzle = SlidingPuzzle(matrizPuzzle)
+		self.miPuzzle = SlidingPuzzle(matrizPuzzle, 3)
 	
 	def test_mostrar_SlidingPuzzle(self):
 		print("Inicio")
@@ -13,10 +13,12 @@ class test_SlidingPuzzle(unittest.TestCase):
 
 	def test_realizar_intercambio_hacia_abajo(self):
 		self.miPuzzle.down(0,1)
-		print("intercambio abajo")
-		self.miPuzzle.mostrarPuzzle()
 
 	def test_realizar_intercambio_hacia_izquierda(self):
 		self.miPuzzle.left(0,1)
-		print("intercambio izquierda")
-		self.miPuzzle.mostrarPuzzle()
+		
+	def test_realizar_intercambio_hacia_derecha(self):
+		self.miPuzzle.right(0,1)
+
+	def test_realizar_intercambio_hacia_arriba(self):
+		self.miPuzzle.up(0,1)
