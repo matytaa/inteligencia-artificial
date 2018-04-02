057baa3fb7025acf5f4c487333e5f93b46ca8f1e
Matias Nievas Soto <matytaa@gmail.com>
2018-04-02 17:05:40 -0300

    implementación método left()

--- Sliding-Puzzle/SlidingPuzzle.py
+++ Sliding-Puzzle/SlidingPuzzle.py
@@ -15,3 +15,8 @@ class SlidingPuzzle:
 		auxiliar = self.matrizPuzzle[fila+1][columna]
 		self.matrizPuzzle[fila+1][columna] = 0
 		self.matrizPuzzle[fila][columna] = auxiliar
+
+	def left(self, fila, columna):
+		auxiliar = self.matrizPuzzle[fila][columna-1]
+		self.matrizPuzzle[fila][columna-1] = 0
+		self.matrizPuzzle[fila][columna] = auxiliar
