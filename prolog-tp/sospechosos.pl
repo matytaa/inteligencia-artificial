7358d70fdf1d61915fcc2469a02d9f619dca714c
Matias Nievas Soto <matytaa@gmail.com>
2018-04-29 00:16:52 -0300

    corrigiendo falta de negación

--- prolog-tp/sospechosos.pl
+++ prolog-tp/sospechosos.pl
@@ -4,12 +4,12 @@ vive_en_la_mansion(carnicero).
 
 vive_en_la_mansion(charles).
 
-la_persona_odia_a(agatha,Persona):- vive_en_la_mansion(Persona),not(Persona==carnicero).
+la_persona_odia_a(charles,Persona):- vive_en_la_mansion(Persona), not(la_persona_odia_a(agatha,Persona)).
 
-la_persona_odia_a(charles,Persona):- vive_en_la_mansion(Persona),not(la_persona_odia_a(agatha,Persona)).
+la_persona_odia_a(agatha,Persona):- vive_en_la_mansion(Persona), not(Persona==carnicero).
 
-la_persona_odia_a(carnicero,Persona):- vive_en_la_mansion(Persona),la_persona_odia_a(agatha,Persona).
+la_persona_odia_a(carnicero,Persona):- vive_en_la_mansion(Persona), la_persona_odia_a(agatha,Persona).
 
 es_mas_rico_que(Persona, agatha):- not(la_persona_odia_a(carnicero,Persona)),vive_en_la_mansion(Persona).
 
-el_asesino_es(Persona, agatha):- la_persona_odia_a(Persona,agatha),es_mas_rico_que(Persona, agatha).
+el_asesino_es(Persona, agatha):- la_persona_odia_a(Persona,agatha), not(es_mas_rico_que(Persona, agatha)).
